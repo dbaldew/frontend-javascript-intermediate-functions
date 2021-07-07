25c9269ee7eb6b8a8ddcc8c14f0852b2f1d7caee
dbaldew <dbaldew@gmail.com>
2021-07-07 23:38:13 +0200

    update

--- functions-methods.js
+++ functions-methods.js
@@ -1,7 +1,6 @@
 // Je gaat functies schrijven die we kunnen hergebruiken om sommige emailadressen te checken. Nu zul je gaan merken hoe handig functies kunnen zijn!
 // Je zult hier methoden van het String Object voor nodig hebben, dus pak de paragraaf op EdHub over het String Object er even bij.
 
-
 /* Opdracht  1 */
 // Schrijf een functie genaamd getEmailDomain, die een emailadres verwacht en de domeinnaam teruggeeft. Een domeinnaam is hetgeen dat na het @ in het adres staat
 // ---- Verwachte uitkomsten:
@@ -9,8 +8,16 @@
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
+// let email  = "n.eeken@novi-education.nl";
+// let domain = email.substring(email.lastIndexOf("@")+1)
+// console.log(domain);
 
-
+let domain;
+function getEmailDomain (email){
+    return domain = email.substring(email.lastIndexOf("@") +1);
+}
+domain = getEmailDomain("t.mellink@novi.nl");
+console.log(domain);
 
 /* Opdracht  2 */
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
@@ -20,7 +27,21 @@
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
+function typeOfEmail (email){
+    domain = email.substring(email.lastIndexOf("@") +1);
+        if (domain === "novi-education.nl"){
+            return "Student"
+        }
+        if (domain === "novi.nl"){
+            return "Medewerker"
+        }
+        else {
+            return "Extern"
+        }
+}
 
+let type = typeOfEmail("a.wiersma@outlook.com");
+console.log(type)
 
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
@@ -34,3 +55,13 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+// dit is niet gelukt , kom er niet uit - db
+
+let valid = false
+function emailValidity (email) {
+
+}
+
+valid = emailValidity("n.eeken@novi.nl");
+console.log(valid);
